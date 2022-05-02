e78b20c2c9555d7d66ce9d943fdc8d3766eedce0
Oleksandr Pichkurov <oleksandr.pichkurov@getoxygen.com>
2022-05-02 18:46:54 +0300

    some improves

--- src/handlers.rs
+++ src/handlers.rs
@@ -22,46 +22,50 @@ pub fn handle_connection(mut stream: TcpStream, folder: &String) {
     let mut filename = false;
 
     let response = if buffer.starts_with(main_page) {
-        handle_response(OK, "hello.html")
+        Response::new(OK, "hello.html")
     } else if buffer.starts_with(ss.as_bytes()){
         filename = true;
-        format!(
-            "{}\r\nContent-Disposition: attachment\r\nContent-Length:\r\n\r\n", OK
-        )
+        Response::new(OK, "")
     } else {
         log("url shoud start with root path with value wich you pass to params".to_string());
-        handle_response(BAD_REQUEST, "400.html")
+        Response::new(BAD_REQUEST, "400.html")
     };
 
     if filename {
         let vec_of_folders  = resource.folder_contents().unwrap();
         if let Some(path_to_file) = vec_of_folders.iter().position(|x| x.ends_with(file_name)){
             let contents = fs::read(vec_of_folders.get(path_to_file).unwrap()).unwrap();
-            stream.write(response.as_bytes()).unwrap();
-            stream.write_all(&contents).unwrap();
+            stream.write(response.response.as_bytes()).unwrap();
+            contents.into_iter().for_each( |e| {stream.write(&[e]).unwrap();});
             stream.flush().unwrap()
         } else {
-            let response = handle_response(NOT_FOUND, "404.html");
-            stream.write(response.as_bytes()).unwrap();
+            let response = Response::new(NOT_FOUND, "404.html");
+            stream.write(response.response.as_bytes()).unwrap();
             stream.flush().unwrap()
         }
     }
-    stream.write(response.as_bytes()).unwrap();
+    stream.write(response.response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
-fn handle_response(status_line: &str, filename: &str) -> String{
-    if filename.len() > 0{
-        let contents = fs::read_to_string(filename).unwrap();
-        format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        )
-    } else {
-        format!(
-            "{}\r\nContent-Disposition: attachment\r\nContent-Length: \r\n\r\n", OK
-        )
+struct Response {
+    response: String
+}
+
+impl Response {
+    fn new(status_line: &str, filename: &str) -> Self {
+        if filename.len() > 0{
+            let contents = fs::read_to_string(filename).unwrap();
+            Self {response: format!(
+                "{}\r\nContent-Length: {}\r\n\r\n{}",
+                status_line,
+                contents.len(),
+                contents
+            )}
+        } else {
+            Self { response: format!(
+                "{}\r\nContent-Disposition: attachment\r\nContent-Length: \r\n\r\n", OK
+            )}
+        }
     }
 }
